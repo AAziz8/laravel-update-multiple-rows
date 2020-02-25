ba67490354157ee7ff2fa11c093287197c6e04c6
RI Rijan <38884716+DeveloperRijan@users.noreply.github.com>
2020-02-25 19:52:34 +0600

    Update sample_code.php

--- sample_code.php
+++ sample_code.php
@@ -1,23 +1,29 @@
 <?php
+use DataTable;
 
 //first make array of your id that which are you want to update
+public function myFunc(){
+	$id_list = [1, 2, 3, 4, 5];
+	//then do like below suppose your model is 'DataTable'
+	DataTable::whereIn('id', $id_list)
+	    ->update([
+		'colour' => 'black',
+		'size' => 'XL', 
+		'price' => 10000
+	    ]);
 
-$id_list = [1, 2, 3, 4, 5];
-
-//then do like below suppose your model is 'DataTable'
-DataTable::whereIn('id', $id_list)
-    ->update([
-        'colour' => 'black',
-        'size' => 'XL', 
-        'price' => 10000
-    ]);
+}
 	
 
 
 //even you can get data from array of id 
+public function myFunc2(){
 
-$id_list = [1, 2, 3, 4, 5];
+	$id_list = [1, 2, 3, 4, 5];
 
-DataTable::whereIn('id', $id_list)
+	DataTable::whereIn('id', $id_list)
 			->with('category')
 			->get();
+}
+
+
